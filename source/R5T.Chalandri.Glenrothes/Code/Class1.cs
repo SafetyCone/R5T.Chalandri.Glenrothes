8f15fd89809877086bde9cb7928147fc906f87f4
MinexAutomation <minex.automation@gmail.com>
2020-01-22 15:04:32 -0800

    Added TestingDataDirectoryContentPathsProvider implementation of ITestingDataDirectoryContentPathsProvider service.

--- source/R5T.Chalandri.Glenrothes/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Chalandri.Glenrothes
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Chalandri.Glenrothes/Code/Services/Implementations/TestingDataDirectoryContentPathsProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Glenrothes;
+using R5T.Ilioupoli;
+using R5T.Lombardy;
+
+
+namespace R5T.Chalandri.Glenrothes
+{
+    public class TestingDataDirectoryContentPathsProvider : ITestingDataDirectoryContentPathsProvider
+    {
+        private ITestingDataDirectoryPathProvider TestingDataDirectoryPathProvider { get; }
+        private ITestingDataDirectoryContentConventions TestingDataDirectoryContentConventions { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public TestingDataDirectoryContentPathsProvider(
+            ITestingDataDirectoryPathProvider testingDataDirectoryPathProvider,
+            ITestingDataDirectoryContentConventions testingDataDirectoryContentConventions,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.TestingDataDirectoryPathProvider = testingDataDirectoryPathProvider;
+            this.TestingDataDirectoryContentConventions = testingDataDirectoryContentConventions;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetBasicTextFilePath()
+        {
+            var testingDataDirectoryPath = this.TestingDataDirectoryPathProvider.GetTestingDataDirectoryPath();
+
+            var basicTextFileName = this.TestingDataDirectoryContentConventions.BasicTextFileName;
+
+            var basicTextFilePath = this.StringlyTypedPathOperator.GetFilePath(testingDataDirectoryPath, basicTextFileName);
+            return basicTextFilePath;
+        }
+    }
+}
