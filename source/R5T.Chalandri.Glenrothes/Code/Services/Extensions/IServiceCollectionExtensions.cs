f149bf69d00969ffa01fc214fbb600b5783ead1e
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:22:17 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

---
+++ source/R5T.Chalandri.Glenrothes/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Glenrothes;
+using R5T.Ilioupoli;
+using R5T.Lombardy;
+
+
+namespace R5T.Chalandri.Glenrothes
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TestingDataDirectoryContentPathsProvider"/> implementation of <see cref="ITestingDataDirectoryContentPathsProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddGlenrothesTestingDataDirectoryContentPathsProvider(this IServiceCollection services,
+            ServiceAction<ITestingDataDirectoryPathProvider> addTestingDataDirectoryPathProvider,
+            ServiceAction<ITestingDataDirectoryContentConventions> addTestingDataDirectoryContentConventions,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<ITestingDataDirectoryContentPathsProvider, TestingDataDirectoryContentPathsProvider>()
+                .RunServiceAction(addTestingDataDirectoryPathProvider)
+                .RunServiceAction(addTestingDataDirectoryContentConventions)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="TestingDataDirectoryContentPathsProvider"/> implementation of <see cref="ITestingDataDirectoryContentPathsProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryContentPathsProvider> AddGlenrothesTestingDataDirectoryContentPathsProviderAction(this IServiceCollection services,
+            ServiceAction<ITestingDataDirectoryPathProvider> addTestingDataDirectoryPathProvider,
+            ServiceAction<ITestingDataDirectoryContentConventions> addTestingDataDirectoryContentConventions,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryContentPathsProvider>(() => services.AddGlenrothesTestingDataDirectoryContentPathsProviderAction(
+                addTestingDataDirectoryPathProvider,
+                addTestingDataDirectoryContentConventions,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
